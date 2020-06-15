67d34c7be27499ad3a79f4ce56f102bdc341083f
mightyjoe781 <sudhanshumohan781@gmail.com>
2020-06-15 23:22:38 +0530

    need to fix body-parser

--- app.js
+++ app.js
@@ -1,19 +1,8 @@
 var express = require("express"),
 	app = express(),
-	mongoose = require("mongoose"),
 	bodyParser = require("body-parser"),
+	mongoose = require("mongoose"),
 	ESRI = require("./models/esri.js")
-
-//==========================
-//SOME OTHER PACKAGES CONFIG
-//==========================
-
-app.set("view engine","ejs");
-//setting view engine to ejs
-app.use(express.static(__dirname + '/public'));
-//serving custom resources
-app.use(bodyParser.urlencoded({extended:true}));
-//using bodyparser so recieve req object
 
 //MONGO CONFIGRATION
  var uri = process.env.DATABASEURI || "mongodb+srv://sudhanshumohan:hesoyam@cluster0-3z3hj.mongodb.net/hospital_data?retryWrites=true&w=majority"
@@ -28,14 +17,28 @@ mongoose.connect(uri,{
 	console.log("ERROR:",err.message);
 });
 
+//==========================
+//SOME OTHER PACKAGES CONFIG
+//==========================
+
+app.set("view engine","ejs");
+//setting view engine to ejs
+app.use(express.static(__dirname + '/public'));
+//serving custom resources
+app.use(bodyParser.urlencoded({extended:true}));
+//using bodyparser so recieve req object
+
 //Serving HomePage
 app.get('/',function(req,res){
 	res.render("index");
 })
 
 app.post("/",function(req,res){
-	res.send("WORKED");
-});
+	var X = req.body.X;
+	var Y = req.body.Y;
+
+	res.send(req.body);
+	});
 
 //LISTENER PROCESS
 var port = process.env.PORT || 31000
