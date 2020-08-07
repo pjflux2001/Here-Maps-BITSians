e84dac4bd4e987443ba22449cced13443d0d44b2
pjflux2001 <pjflxu2001@gmail.com>
2020-08-07 09:53:17 +0530

    dump grab using curl n sh

---
+++ faq.sh
@@ -0,0 +1,4 @@
+mkdir ./faq_dump
+curl https://app.scrapinghub.com/api/v2/datasets/PNx04znuYbg/download?id=0 -o ./faq_dump/faq1.json -s
+curl https://app.scrapinghub.com/api/v2/datasets/vUUrtEFg7wa/download?format=json -o ./faq_dump/faq2.json -s
+curl https://app.scrapinghub.com/api/v2/datasets/KzknKppxi9F/download?format=json -o ./faq_dump/faq3.json -s
