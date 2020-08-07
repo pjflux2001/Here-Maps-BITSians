a289455cf01ac3a2d90c5c01ddb7d105c32827ed
pjflux2001 <46859995+pjflux2001@users.noreply.github.com>
2020-08-07 09:54:53 +0530

    Update faq.sh

--- faq.sh
+++ faq.sh
@@ -1,3 +1,4 @@
+#! /bin/bash
 mkdir ./faq_dump
 curl https://app.scrapinghub.com/api/v2/datasets/PNx04znuYbg/download?id=0 -o ./faq_dump/faq1.json -s
 curl https://app.scrapinghub.com/api/v2/datasets/vUUrtEFg7wa/download?format=json -o ./faq_dump/faq2.json -s
