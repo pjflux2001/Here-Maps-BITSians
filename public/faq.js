b0669589ee6119e6566a8dda4aa74bce8d0f46b4
Apoorv Garg <40639806+ApEdu@users.noreply.github.com>
2020-08-08 19:21:06 +0530

    FAQ Page Done
    Pagination not done

--- public/faq.js
+++ public/faq.js
@@ -1,4 +1,57 @@
+
 fetch("./faq_dump/faq1.json")
     .then(response => response.text())
-    .then(results => console.log(results))
+    .then(results => {
+        let requiredRegex = /\{([^}]+)\}/g
+        var faqData = results.match(requiredRegex);
+        console.log(faqData);
+        for(var i=0;i<faqData.length;i++){
+            var data = JSON.parse(faqData[i]);
+            console.log()
+            document.getElementById("faqContent").innerHTML  += `
+            <div class="ui accordion">
+                <div class="title"><i class="dropdown icon"></i>${data.question}</div>
+                <div class="content">${data.answer}</div>
+            </div>
+            `
+        } 
+    })
+    .catch(error => console.log('error', error));
+
+fetch("./faq_dump/faq2.json")
+    .then(response => response.text())
+    .then(results => {
+        let requiredRegex = /\{([^}]+)\}/g
+        var faqData = results.match(requiredRegex);
+        console.log(faqData);
+        for(var i=0;i<faqData.length;i++){
+            var data = JSON.parse(faqData[i]);
+            console.log()
+            document.getElementById("faqContent").innerHTML  += `
+            <div class="ui accordion">
+                <div class="title"><i class="dropdown icon"></i>${data.question}</div>
+                <div class="content">${data.answer}</div>
+            </div>
+            `
+        } 
+    })
+    .catch(error => console.log('error', error));
+
+fetch("./faq_dump/faq3.json")
+    .then(response => response.text())
+    .then(results => {
+        let requiredRegex = /\{([^}]+)\}/g
+        var faqData = results.match(requiredRegex);
+        console.log(faqData);
+        for(var i=0;i<faqData.length;i++){
+            var data = JSON.parse(faqData[i]);
+            console.log()
+            document.getElementById("faqContent").innerHTML  += `
+            <div class="ui accordion">
+                <div class="title"><i class="dropdown icon"></i>${data.question}</div>
+                <div class="content">${data.answer}</div>
+            </div>
+            `
+        } 
+    })
     .catch(error => console.log('error', error));
