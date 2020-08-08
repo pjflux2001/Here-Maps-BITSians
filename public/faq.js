9295f462fd21a3e57971fb260db0667080f2fa7d
pjflux2001 <46859995+pjflux2001@users.noreply.github.com>
2020-08-08 14:15:01 +0530

    Update faq.js

--- public/faq.js
+++ public/faq.js
@@ -1,16 +1,4 @@
-$(function() {
-    $.getJSON("./faq_dump/faq1.json",
-       function(json){
-
-  var requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-  };
-  var i;
-  fetch("./faq_dump/faq1.json", requestOptions)
-    .then(response => response.json())
+fetch("./faq_dump/faq1.json")
+    .then(response => response.text())
     .then(results => console.log(results))
     .catch(error => console.log('error', error));
-   }
-  );
-  });
