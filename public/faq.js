d3a8e5fbae64cb9f950fb3bda0d3b6162f736d5d
Apoorv Garg <40639806+ApEdu@users.noreply.github.com>
2020-08-08 19:56:37 +0530

    Modified faq and other design

--- public/faq.js
+++ public/faq.js
@@ -8,50 +8,66 @@ fetch("./faq_dump/faq1.json")
         for(var i=0;i<faqData.length;i++){
             var data = JSON.parse(faqData[i]);
             console.log()
-            document.getElementById("faqContent").innerHTML  += `
-            <div class="ui accordion">
-                <div class="title"><i class="dropdown icon"></i>${data.question}</div>
-                <div class="content">${data.answer}</div>
-            </div>
-            `
+            if(removeTags(data.answer)){
+                document.getElementById("faqContent").innerHTML  += `
+                <div class="ui accordion">
+                    <div class="title"><i class="dropdown icon"></i>${data.question}</div>
+                    <div class="content">${data.answer}</div>
+                </div>
+                `
+            }
         } 
     })
     .catch(error => console.log('error', error));
+
 
 fetch("./faq_dump/faq2.json")
-    .then(response => response.text())
-    .then(results => {
-        let requiredRegex = /\{([^}]+)\}/g
-        var faqData = results.match(requiredRegex);
-        console.log(faqData);
-        for(var i=0;i<faqData.length;i++){
-            var data = JSON.parse(faqData[i]);
-            console.log()
+.then(response => response.text())
+.then(results => {
+    let requiredRegex = /\{([^}]+)\}/g
+    var faqData = results.match(requiredRegex);
+    console.log(faqData);
+    for(var i=0;i<faqData.length;i++){
+        var data = JSON.parse(faqData[i]);
+        console.log()
+        if(removeTags(data.answer)){
             document.getElementById("faqContent").innerHTML  += `
             <div class="ui accordion">
                 <div class="title"><i class="dropdown icon"></i>${data.question}</div>
                 <div class="content">${data.answer}</div>
             </div>
             `
-        } 
-    })
-    .catch(error => console.log('error', error));
+        }
+    } 
+})
+.catch(error => console.log('error', error));
 
 fetch("./faq_dump/faq3.json")
-    .then(response => response.text())
-    .then(results => {
-        let requiredRegex = /\{([^}]+)\}/g
-        var faqData = results.match(requiredRegex);
-        console.log(faqData);
-        for(var i=0;i<faqData.length;i++){
-            var data = JSON.parse(faqData[i]);
-            console.log()
+.then(response => response.text())
+.then(results => {
+    let requiredRegex = /\{([^}]+)\}/g
+    var faqData = results.match(requiredRegex);
+    console.log(faqData);
+    for(var i=0;i<faqData.length;i++){
+        var data = JSON.parse(faqData[i]);
+        console.log()
+        if(removeTags(data.answer)){
             document.getElementById("faqContent").innerHTML  += `
             <div class="ui accordion">
                 <div class="title"><i class="dropdown icon"></i>${data.question}</div>
                 <div class="content">${data.answer}</div>
             </div>
             `
-        } 
-    })
-    .catch(error => console.log('error', error));
+        }
+    } 
+})
+.catch(error => console.log('error', error));
+
+// Function to remove unnecessary tags from scrapped data    
+function removeTags(str) { 
+    if ((str===null) || (str==='')) 
+        return false; 
+    else
+        str = str.toString(); 
+    return str.replace( /(<([^>]+)>)/ig, ''); 
+} 
